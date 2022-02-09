d7103869cad1f1cc125ccf62616c06d3e25c73c7
mehrabriyan <mehraabriyan@gmail.com>
2022-02-09 23:44:30 +0600

    2nd

--- src/components/Header/Head.js
+++ src/components/Header/Head.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import { Button, Form, FormControl } from 'react-bootstrap'
 import './Header.css'
+import bg from '../../images/bannerbackground.png'
+
 function Head() {
   return (
-    <div className='head'>
-        <h1>Best food waiting for your bally</h1>
+    <div className='bg'>
+      <div className='head'>
+        <h1>Best food waiting for your belly</h1>
         <div>
-        <Form className="d-flex Searchform">
-        <FormControl
-          type="search"
-          placeholder="Search"
-          className="me-0 SearchBar"
-          aria-label="Search"
-        />
-        <Button variant="danger" className='SearchBtn'>Sign up</Button>
-      </Form>
+          <Form className="d-flex Searchform">
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-0 SearchBar"
+              aria-label="Search"
+            />
+            <Button variant="danger" className='SearchBtn'>Sign up</Button>
+          </Form>
         </div>
 
+      </div>
     </div>
+
   )
 }
 
