ce4234b5d786535c01fac5319daaff714e4c91e9
mehrabriyan <mehraabriyan@gmail.com>
2022-02-12 14:18:10 +0600

    responsive footer done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,54 @@
 import React from 'react'
-import { Col, Row } from 'react-bootstrap'
+import { Col, Row, Container } from 'react-bootstrap'
+import './Footer.css'
+import logo2 from '../../images/logo.png'
 
 function Footer() {
   return (
-    <div className='footer'>
+    <div className='Container'>
+      <Container>
         <Row>
-            <Col md={6}>
-            </Col>
-            <Col md={6}>
-            </Col>
+          <Col sm={6}>
+            <img src={logo2} alt="logo" className='logo2' />
+          </Col>
+          <Col sm={6}>
+            <div className='footCon'>
+              <Col sm={6} >
+                <ul className='listFoot'>
+                  <li>About Online food</li>
+                  <li> Read our blog</li>
+                  <li> Sign uo to deliver</li>
+                  <li> Add your restaurant</li>
+                </ul>
+              </Col>
+              <Col sm={6}>
+                <ul className='listFoot'>
+                  <li>About Online food</li>
+                  <li> Read our blog</li>
+                  <li> Sign uo to deliver</li>
+                  <li> Add your restaurant</li>
+                </ul>
+              </Col>
+            </div>
+
+          </Col>
         </Row>
+        <Row>
+          <div className='resFoot'>
+            <Col sm={8} >
+              <p style={{ fontSize: '15px' }}>Copyright @ 2022 Riyan Hossain</p>
+            </Col>
+            <Col sm={4} className='policy'>
+              <a href='#'>Privacy Policy</a>
+              <a href='#'>Terms of use</a>
+              <a href='#'>Pricing</a>
+            </Col>
+
+          </div>
+
+        </Row>
+      </Container>
+
     </div>
   )
 }
